77c57d63299db074bb3a0820324a9981051399df
Andy Robinson <andy@opentrack.run>
2025-04-03 22:40:09 +0100

    adding a static file

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,15 +120,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_HOST = "https://dos3b38mjx5bt.cloudfront.net" if not DEBUG else ""
-STATIC_URL = STATIC_HOST + "/static/"
+# STATIC_HOST = "https://dos3b38mjx5bt.cloudfront.net" if not DEBUG else ""
+# STATIC_URL = STATIC_HOST + "/static/"
+
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "mysite/static"
 
 STORAGES = {
     # ...
