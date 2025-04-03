d7311ec7e60ba6c154ed3c830f09ecd51f906e17
Llewellyn Brunsdon-Haland <llewellynbh@gmail.com>
2025-04-03 17:34:50 +0100

    fixing cloudfront url

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_HOST = "https://dos3b38mjx5bt.cloudfront.net." if not DEBUG else ""
+STATIC_HOST = "https://dos3b38mjx5bt.cloudfront.net" if not DEBUG else ""
 STATIC_URL = STATIC_HOST + "/static/"
 
 # Default primary key field type
